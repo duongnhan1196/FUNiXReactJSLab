8097d6772ab194e006fcb51ac17962b12c6020ca
Duong Nhan <nhandtFX13325@funix.edu.vn>
2022-03-27 01:09:48 +0700

    complete exercise1

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, CardGroup } from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
@@ -19,29 +19,25 @@ class Menu extends Component {
             <div key={comment.id}>
               
               <p>{comment.comment}</p>
-              <p>-- {comment.author}, </p>
+              <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { month: 'short', day: '2-digit', year: 'numeric' }).format(new Date(comment.date))} </p>
             </div>
             </div>
           );
         });
         return(
-          <Card className="col-12">
-            <Card className="col-6 float-left">
+          <CardGroup className='col-sm-12'>
+            <Card className="col-6 col-sm-12">
             <CardImg width="100%" src={dish.image} alt={dish.name}/>
             <CardBody>
               <CardTitle>{dish.name}</CardTitle>
               <CardText>{dish.description}</CardText>
             </CardBody>
             </Card>
-            <Card className="col-5">
+            <Card className="col-6 col-sm-12">
               <h4>Comments</h4>
               <div className='row'>{cmt}</div>
-              <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-              </CardBody>
             </Card>
-          </Card>          
+          </CardGroup>         
         );
       }
       else {
