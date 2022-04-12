3a2ee368dc7c1dd1b42e6391b2032807545ef756
Duong Nhan <nhandtFX13325@funix.edu.vn>
2022-04-12 23:03:40 +0700

    add Button colum

--- confusion/src/components/StaffListComponent.js
+++ confusion/src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import { Card, CardText, CardTitle, Collapse } from 'reactstrap'; 
+import { Card, CardText, CardTitle, Button, ButtonGroup, ButtonToolbar } from 'reactstrap'; 
 import dateFormat from 'dateformat';
 
 class StaffList extends Component{
@@ -7,12 +7,20 @@ class StaffList extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            selectedStaff: null
-        }
+            selectedStaff: null,
+            coLumDefault: "col-12 col-md-4 col-sm-6 p-1"
+        };
     }
     onStaffSelect(staff) {
         this.setState({ selectedStaff: staff});
     }
+    setColum(col) {
+        this.setState({
+            coLumDefault: col
+        });
+    }
+
+
     renderStaff(staff) {
         if (staff != null) {
             return (
@@ -25,8 +33,7 @@ class StaffList extends Component{
                         <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
                         <CardText>Phòng ban: {staff.department.name}</CardText>
                         <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-                        <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-                        
+                        <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>     
                     </Card>
             );
         }
@@ -40,7 +47,7 @@ class StaffList extends Component{
     render() {
         const stafflist = this.props.staffs.map((staff) => {
             return (
-                <div key={staff.id} className="col-md-4 col-sm-6 p-1">
+                <div key={staff.id} className={this.state.coLumDefault}>
                     <Card onClick={() => this.onStaffSelect(staff)} style={{
                         backgroundColor: '#80ffff',
                         borderColor: '#333'
@@ -53,6 +60,27 @@ class StaffList extends Component{
 
         return (
             <div className='container'>
+                <div className='row'>
+                    <CardTitle className={"col-md-2 mt-1"}>
+                        Số cột hiển thị :
+                    </CardTitle>
+                    <ButtonToolbar>
+                    <ButtonGroup >
+                        <Button onClick={() => this.setColum("col-md-12 mt-1")} color="secondary" outline>
+                            1
+                        </Button>
+                        <Button onClick={() => this.setColum("col-md-6 mt-1")} color="secondary" outline>
+                            2
+                        </Button>
+                        <Button onClick={() => this.setColum("col-md-4 mt-1")} color="secondary" outline>
+                            3
+                        </Button>
+                        <Button onClick={() => this.setColum("col-md-2 mt-1")} color="secondary" outline>
+                            6 
+                        </Button>
+                    </ButtonGroup>
+                    </ButtonToolbar>
+                </div>
                 <div className='row'>
                     {stafflist}
                 </div>
