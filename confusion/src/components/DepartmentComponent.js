fae69a2fea852791a3a964e57cab6f0f129fc802
Duong Nhan <nhandtFX13325@funix.edu.vn>
2022-04-22 23:18:59 +0700

    add Breadcrumb to DepartmentComponent

--- confusion/src/components/DepartmentComponent.js
+++ confusion/src/components/DepartmentComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, CardText, CardTitle } from 'reactstrap';
+import { Card, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDepartment({ department }) {
     return (
@@ -22,6 +23,12 @@ function Department(props) {
     return (
         <div style={{ backgroundColor: '#d1e0e0' }}>
             <div className="container">
+                <div className='row'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/staffs'>Nhân Viên</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Phòng Ban</BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
                 <div className="row">{department}</div>
             </div>
         </div>
