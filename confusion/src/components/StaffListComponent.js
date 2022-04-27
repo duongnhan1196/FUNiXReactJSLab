ad34559fb85b839052ddf77fc9f717c5f9b18e37
Duong Nhan <nhandtFX13325@funix.edu.vn>
2022-04-27 23:46:10 +0700

    adding Button Search

--- confusion/src/components/StaffListComponent.js
+++ confusion/src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, CardText,  CardImg, CardBody  } from 'reactstrap'; 
+import React, { Component } from 'react';
+import { Card, CardText, CardImg, CardBody, Form, Input, Button, FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
@@ -10,46 +10,82 @@ function RenderCard({ staff }) {
                 <Link to={`/staffs/${staff.id}`}>
                     <CardImg src={staff.image} alt={staff.name} />
                 </Link>
-                     <CardBody>
-                        <CardText className="text-center">{staff.name}</CardText>
-                    </CardBody>
-   
+                <CardBody>
+                    <CardText className="text-center">{staff.name}</CardText>
+                </CardBody>
+
             </Card>
         </div>
-        
+
     );
 }
 
+class StaffList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameSearch: ""
+        };
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+    handleSearch(event) {
+        event.preventDefault();
+        const keySearch = event.target.keySearch.value;
+        this.setState({ nameSearch: keySearch });
+    }
 
 
-const StaffList = (props) => {
-    const staff = props.staffs.map((staff) => {
+    render() {
+
+        const staff = this.props.staffs.filter((val) => {
+            if (this.state.nameSearch === "") return val;
+            else if (
+                val.name.toLowerCase().includes(this.state.nameSearch.toLowerCase())
+            )
+                return val;
+            return 0;
+        })
+            .map((val) => {
+                return (
+                    <div key={val.id} className="col-6 col-md-2 col-sm-4 p-2">
+                        <RenderCard staff={val} />
+                    </div>
+                );
+            });
+
         return (
-            <div key={staff.id} className="col-6 col-md-2 col-sm-4 p-2">
-                <RenderCard staff={staff} />
-            </div>
-        );
-    });
+            <div style={{ backgroundColor: '#b8e6f0' }}>
+                <div className="container" style={{ backgroundColor: '#ccffff' }} >
+                    <div className='row'>
+                        <div className='col-4'>
+                            <h3>Nhân viên</h3>
+                            <hr />
+                        </div>
+                        <Form className='col-8' onSubmit={this.handleSearch}>
+                            <FormGroup className="row">
+                                <Input
+                                    type="text"
+                                    id="keySearch"
+                                    name="keySearch"
+                                    placeholder="Tìm kiếm nhân viên"
+                                    className="col-10 m-1"
+                                />
+                                <Button type="submit"><span className="fa fa-search fa-lg"></span> Tìm kiếm</Button>
+                                <Button type="submit" color="primary"><span className="fa fa-plus fa-lg"></span> Thêm Nhân Viên</Button>
+                            </FormGroup>
 
-    return (
-        <div style={{ backgroundColor: '#b8e6f0' }}>
-            <div className="container" style={{ backgroundColor: '#ccffff' }} >
-                <div className='row'>
-                    <div className='col-12'>
-                        <h3>Nhân viên</h3>
-                        <hr />
+
+                        </Form>
+                    </div>
+                    <div className="row">
+                        {staff}
                     </div>
                 </div>
-                <div className="row">       
-                        {staff}            
-                </div>
             </div>
-        </div>
-       
-    );
+
+        );
+    }
 }
-
-
 
 
 export default StaffList;
